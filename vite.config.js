8cbdd85ca2d9c5765a3aa22ca0cfc5e209c0cb5e
RostNshka <devkotder@ya.ru>
2026-03-12 18:19:00 +0400

    added rules for scss import

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,13 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use '@/styles/helpers' as *;
+        `,
+      },
+    },
+  },
 })
