4c1ea6994110d0ae690d20334b2cb11bc7394c86
RostNshka <devkotder@ya.ru>
2026-03-12 14:11:59 +0400

    added eslint, stylelint, prettier, and setting configs

---
+++ eslint.config.js
@@ -0,0 +1,41 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import pluginReact from 'eslint-plugin-react'
+import { defineConfig } from 'eslint/config'
+import prettierPlugin from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    plugins: {
+      js,
+      react: pluginReact,
+      prettier: prettierPlugin,
+    },
+    extends: [
+      js.configs.recommended,
+      pluginReact.configs.flat.recommended,
+      prettierConfig,
+    ],
+    rules: {
+      ...prettierPlugin.configs.recommended.rules,
+      'no-console': 'warn',
+      eqeqeq: 'warn',
+      curly: 'warn',
+      'no-else-return': 'warn',
+      'react/react-in-jsx-scope': 'off',
+    },
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+  },
+])
