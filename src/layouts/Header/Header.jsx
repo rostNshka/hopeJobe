5f09e005a33e7e712b963b8a5d851663266781d9
RostNshka <devkotder@ya.ru>
2026-03-16 22:06:39 +0400

    added event when we work with dialog window in tablet and mobile

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -2,12 +2,27 @@ import './Header.scss'
 import { NavLink } from 'react-router-dom'
 import Button from '@/components/Button'
 import BurgerButton from '@/components/BurgerButton'
+import { useEffect, useRef, useState } from 'react'
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const ref = useRef(null)
+  useEffect(() => {
+    if (isOpen) {
+      ref.current?.show()
+    } else {
+      ref.current?.close()
+    }
+  }, [isOpen])
+
+  const handleClick = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
     <header className="header">
       <img src="/logo.svg" alt="logo" className="header__logo" />
-      <dialog className="header__overlay-menu-dialog" open>
+      <dialog className="header__overlay-menu-dialog" ref={ref}>
         <nav className="header__nav">
           <NavLink to={'/'} className="header__nav-item">
             Вакансии
@@ -22,7 +37,10 @@ const Header = () => {
         </nav>
         <Button color="violet">Войти</Button>
       </dialog>
-      <BurgerButton className="header__burger-button visible-tablet" />
+      <BurgerButton
+        className={`header__burger-button visible-tablet ${isOpen ? 'is-active' : ''}`}
+        onClick={handleClick}
+      />
     </header>
   )
 }
