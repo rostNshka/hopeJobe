669628e69544bb158a94f827b7244e836e4a4a96
RostNshka <devkotder@ya.ru>
2026-03-16 19:29:14 +0400

    add Header component

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,13 +1,29 @@
 import './Header.scss'
 import { NavLink } from 'react-router-dom'
+import Button from '@/components/Button'
+import BurgerButton from '@/components/BurgerButton'
 
 const Header = () => {
   return (
-    <>
-      <NavLink to={'/'}>Вакансии</NavLink>
-      <NavLink to={'/favorites'}>Избранное</NavLink>
-      <NavLink to={'/add-vacancy'}>Добавить вакансию</NavLink>
-    </>
+    <header className="header">
+      <img src="/logo.svg" alt="logo" className="header__logo" />
+      <dialog className="header__overlay-menu-dialog" open>
+        <nav className="header__nav">
+          <NavLink to={'/'} className="header__nav-item">
+            Вакансии
+          </NavLink>
+          <NavLink to={'/favorites'} className="header__nav-item">
+            Избранное
+          </NavLink>
+          <NavLink to={'/add-vacancy'} className="header__nav-item">
+            Новая вакансия
+          </NavLink>
+          <span></span>
+        </nav>
+        <Button color="violet">Войти</Button>
+      </dialog>
+      <BurgerButton className="header__burger-button visible-tablet" />
+    </header>
   )
 }
 
