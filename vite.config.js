201b662d9866175cfd6c3ea4bdd02437996bf1b5
rostNshka <devkotder@ya.ru>
2026-04-17 17:19:46 +0400

    add proxy in vite

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,15 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4200',
+        changeOrigin: true,
+        rewrite: (path) => path,
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
