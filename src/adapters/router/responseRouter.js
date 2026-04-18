a81c4a48b9a108d7f62ea4c2c82c412bb7dcc60c
rostNshka <devkotder@ya.ru>
2026-04-18 22:53:41 +0400

    update responseRouter

--- src/adapters/router/responseRouter.js
+++ src/adapters/router/responseRouter.js
@@ -1,26 +1,40 @@
 import useFetch from '@/adapters/api/useFetch'
 
-function useGetResponses() {
-  return useFetch(' http://localhost:4200/api/responses', { method: 'GET' })
-}
-
-function useAddResponses() {
-  return useFetch(' http://localhost:4200/api/responses', { method: 'POST' })
-}
-
-function useDeleteResponses() {
-  return useFetch(' http://localhost:4200/api/responses', { method: 'DELETE' })
-}
-
-function useCheckResponses() {
-  return useFetch(' http://localhost:4200/api/responses/check/:vacancyId', {
+export function useGetResponses() {
+  const { data, loading, error, refetch } = useFetch('/api/responses', {
     method: 'GET',
   })
+
+  return {
+    vacancies: data?.data || [],
+    loading,
+    error,
+    refetch,
+  }
 }
 
-export default {
-  useGetResponses,
-  useAddResponses,
-  useDeleteResponses,
-  useCheckResponses,
+export function useAddResponses() {
+  const { data, loading, error, refetch } = useFetch('/api/responses', {
+    method: 'POST',
+  })
+
+  return {
+    vacancies: data?.data || [],
+    loading,
+    error,
+    refetch,
+  }
+}
+
+export function useDeleteResponses() {
+  const { data, loading, error, refetch } = useFetch('/api/responses', {
+    method: 'DELETE',
+  })
+
+  return {
+    vacancies: data?.data || [],
+    loading,
+    error,
+    refetch,
+  }
 }
