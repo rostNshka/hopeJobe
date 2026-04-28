ffa696a85de555958690267a45953422a551a081
rostNshka <devkotder@ya.ru>
2026-04-28 22:32:23 +0400

    add field component for all input in project

---
+++ src/components/Field/Field.jsx
@@ -0,0 +1,43 @@
+import './Field.scss'
+import { FaRegEye } from 'react-icons/fa6'
+import { useState } from 'react'
+
+const Field = (props) => {
+  const { htmlFor, label, type, id, name, placeholder } = props
+  const [typePassword, setTypePassword] = useState('password')
+
+  const showPassword = () => {
+    setTypePassword('text')
+  }
+
+  const hidePassword = () => {
+    setTypePassword('password')
+  }
+
+  return (
+    <div className={`field__${name}`}>
+      <label htmlFor={htmlFor} className="field__label">
+        {label}
+      </label>
+      <input
+        type={type === 'password' ? typePassword : type}
+        id={id}
+        name={name}
+        placeholder={placeholder}
+        className="field__input"
+      />
+      {id === 'password' && (
+        <button
+          className="field__button"
+          type="button"
+          onMouseDown={showPassword}
+          onMouseUp={hidePassword}
+          onMouseLeave={hidePassword}>
+          <FaRegEye />
+        </button>
+      )}
+    </div>
+  )
+}
+
+export default Field
