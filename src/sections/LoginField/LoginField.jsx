d29407bc7787e323262ba419f1ab35a3618dd70f
rostNshka <devkotder@ya.ru>
2026-04-30 14:12:37 +0400

    swap basic inputs to Field component

--- src/sections/LoginField/LoginField.jsx
+++ src/sections/LoginField/LoginField.jsx
@@ -1,14 +1,13 @@
 import './LoginField.scss'
 import { useState } from 'react'
-import { FaRegEye } from 'react-icons/fa6'
 import { useLogin } from '@/adapters/router/authRouter'
+import Field from '@/components/Field'
 
 const LoginField = ({ onSuccess }) => {
   const [formData, setFormData] = useState({
     email: '',
     password: '',
   })
-  const [typePassword, setTypePassword] = useState('password')
   const [localError, setLocalError] = useState('')
 
   const { login, loading } = useLogin()
@@ -20,14 +19,6 @@ const LoginField = ({ onSuccess }) => {
       [name]: value,
     }))
     setLocalError('')
-  }
-
-  const showPassword = () => {
-    setTypePassword('text')
-  }
-
-  const hidePassword = () => {
-    setTypePassword('password')
   }
 
   const validateForm = () => {
@@ -86,43 +77,26 @@ const LoginField = ({ onSuccess }) => {
 
   return (
     <form id="login-form" onSubmit={handleSubmit}>
-      <div className="login-form__email">
-        <label htmlFor="email" className="login-form__label">
-          Email
-        </label>
-        <input
-          className={`login-form__input ${localError ? 'login-form__input-error' : null}`}
-          type="email"
-          id="email"
-          name="email"
-          placeholder="you@example.com"
-          value={formData.email}
-          onChange={handleChange}
-        />
-      </div>
-
-      <div className="login-form__password">
-        <label htmlFor="password" className="login-form__label">
-          Пароль
-        </label>
-        <input
-          className={`login-form__input ${localError ? 'login-form__input-error' : null}`}
-          type={typePassword}
-          id="password"
-          name="password"
-          placeholder="••••••"
-          value={formData.password}
-          onChange={handleChange}
-        />
-        <button
-          className="login-form__hide-button"
-          type="button"
-          onMouseDown={showPassword}
-          onMouseUp={hidePassword}
-          onMouseLeave={hidePassword}>
-          <FaRegEye />
-        </button>
-      </div>
+      <Field
+        htmlFor="email"
+        label="Email"
+        type="email"
+        id="email"
+        name="email"
+        placeholder="you@example.com"
+        value={formData.email}
+        onChange={handleChange}
+      />
+      <Field
+        htmlFor="password"
+        label="Пароль"
+        type="password"
+        id="password"
+        name="password"
+        placeholder="••••••"
+        value={formData.password}
+        onChange={handleChange}
+      />
       {localError ? (
         <div className="login-form__error">{localError}</div>
       ) : null}
