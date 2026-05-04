5bb5d9d0ee0808f5c2bf99204a69f66bf839852b
rostNshka <devkotder@ya.ru>
2026-05-04 14:44:32 +0400

    rerender when local storage change

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -4,14 +4,27 @@ const useLocalStorage = (key) => {
   const [data, setData] = useState(null)
 
   useEffect(() => {
-    const item = localStorage.getItem(key)
-
-    if (item) {
-      try {
-        setData(JSON.parse(item))
-      } catch {
-        setData(item)
+    const getData = () => {
+      const item = localStorage.getItem(key)
+      if (item) {
+        try {
+          setData(JSON.parse(item))
+        } catch {
+          setData(item)
+        }
+      } else {
+        setData(null)
       }
+    }
+
+    getData()
+
+    window.addEventListener('storage', getData)
+    window.addEventListener('localStorageChange', getData)
+
+    return () => {
+      window.removeEventListener('storage', getData)
+      window.removeEventListener('localStorageChange', getData)
     }
   }, [key])
 
