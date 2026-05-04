60b3ab5862a1da505b6bf3c7d60967894af43f42
rostNshka <devkotder@ya.ru>
2026-05-04 14:46:22 +0400

    add new prop onClick for button

--- src/components/ProfileButton/ProfileButton.jsx
+++ src/components/ProfileButton/ProfileButton.jsx
@@ -1,7 +1,11 @@
 import './ProfileButton.scss'
 
-const ProfileButton = ({ children }) => {
-  return <button className="profile-button">{children}</button>
+const ProfileButton = ({ children, onClick }) => {
+  return (
+    <button className="profile-button" onClick={onClick}>
+      {children}
+    </button>
+  )
 }
 
 export default ProfileButton
