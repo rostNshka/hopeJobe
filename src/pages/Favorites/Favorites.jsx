d77cee3739112ab1e76d0e9788e2b2eef6c09b4e
rostNshka <devkotder@ya.ru>
2026-05-21 21:52:50 +0400

    add circle on page

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -51,6 +51,9 @@ const Favorites = () => {
           />
         ))}
       </div>
+      <div className="circle circle-1"></div>
+      <div className="circle circle-2"></div>
+      <div className="circle circle-3"></div>
     </div>
   )
 }
