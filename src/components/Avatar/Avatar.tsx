fca3ed5490b10be9564c6d3c3017981bd40fea3a
rostNshka <devkotder@ya.ru>
2026-06-30 14:56:23 +0400

    switching from JS to TS for Avatar component

---
+++ src/components/Avatar/Avatar.tsx
@@ -0,0 +1,23 @@
+import './Avatar.scss'
+
+interface AvatarProps {
+  name: string
+}
+
+const Avatar = ({ name }: AvatarProps) => {
+  const matches = name.match(/[A-ZА-Я]/g) || []
+  let initials: string
+  if (matches.length > 0) {
+    initials = matches.slice(0, 2).join('')
+  } else {
+    initials = name.trim()[0] || '?'
+  }
+  const colorIndex = (name.length % 5) + 1
+  return (
+    <div className="avatar" data-color={colorIndex}>
+      {initials}
+    </div>
+  )
+}
+
+export default Avatar
