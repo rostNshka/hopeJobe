523c1bdd788c31140e343f27a61589a94b3ea773
rostNshka <devkotder@ya.ru>
2026-06-30 14:49:31 +0400

    update eslint.config

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import pluginReact from 'eslint-plugin-react'
 import { defineConfig } from 'eslint/config'
 import prettierPlugin from 'eslint-plugin-prettier'
 import prettierConfig from 'eslint-config-prettier'
+import tseslint from 'typescript-eslint'
 
 export default defineConfig([
   {
@@ -33,6 +34,46 @@ export default defineConfig([
         ecmaFeatures: { jsx: true },
         ecmaVersion: 2020,
         sourceType: 'module',
+      },
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      react: pluginReact,
+      prettier: prettierPlugin,
+    },
+    extends: [
+      ...tseslint.configs.recommended,
+      pluginReact.configs.flat.recommended,
+      prettierConfig,
+    ],
+    rules: {
+      ...prettierPlugin.configs.recommended.rules,
+      'no-console': 'warn',
+      eqeqeq: 'warn',
+      curly: 'warn',
+      'no-else-return': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+    languageOptions: {
+      parser: tseslint.parser,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        project: './tsconfig.json',
       },
     },
     settings: {
