63c242ec43af3e8df9700962deb2abb006851174
rostNshka <devkotder@ya.ru>
2026-06-30 15:13:48 +0400

    switching from JS to TS for RoleDescription component

--- src/components/RoleDescription/RoleDescription.tsx
+++ src/components/RoleDescription/RoleDescription.tsx
@@ -1,7 +1,11 @@
 import './RoleDescription.scss'
 import { PiMagicWandThin } from 'react-icons/pi'
 
-const RoleDescription = ({ role }) => {
+interface IRoleDescription {
+  role?: string
+}
+
+const RoleDescription = ({ role }: IRoleDescription) => {
   return (
     <div className={`role-description__${role}`}>
       <PiMagicWandThin className="icon" />
