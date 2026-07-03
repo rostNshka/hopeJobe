c6222cae1c0a37b1809f0e156eb0a070b28e14da
rostNshka <devkotder@ya.ru>
2026-07-03 14:35:03 +0400

    swaped from js to ts component NotFound and reexport file and add button for back to main page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,12 +1,23 @@
 import './NotFound.scss'
 import NotFoundImg from '@/assets/images/detective-check.png'
+import { useNavigate } from 'react-router-dom'
+import { useCallback } from 'react'
 
 const NotFound = () => {
+  const navigate = useNavigate()
+
+  const handleNavigate = useCallback(() => {
+    navigate('/')
+  }, [navigate])
+
   return (
     <div className="not-found">
-      <h2>Страницы не найдена :c </h2>
+      <h2>Страница не найдена :c </h2>
       <img src={NotFoundImg} alt="" />
       <p>Извините, запрашиваемая страница не существует или была перемещена.</p>
+      <button type="button" onClick={handleNavigate}>
+        На главную
+      </button>
       <div className="circle circle-1"></div>
       <div className="circle circle-2"></div>
       <div className="circle circle-3"></div>
