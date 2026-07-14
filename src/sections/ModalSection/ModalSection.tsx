72ab3be44a82e082a29a5d879ec5628fc2437bcb
rostNshka <devkotder@ya.ru>
2026-07-14 15:15:48 +0400

    fix: refactor ModalSection component for improved readability and structure

--- src/sections/ModalSection/ModalSection.tsx
+++ src/sections/ModalSection/ModalSection.tsx
@@ -1,5 +1,5 @@
 import './ModalSection.scss'
-import React, { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { observer } from 'mobx-react-lite'
 import ToggleButton from '@/components/ToggleButton'
 import RoleDescription from '@/components/RoleDescription'
@@ -10,112 +10,114 @@ import roleStore from '@/stores/role-store.tsx'
 import { ModalSectionProps } from './ModalSectionProps'
 import { IUserContextData, IUserData, TRole } from '@/types/entities/user.types'
 
-const ModalSection = ({
-  isOpen,
-  onClose,
-  onLoginSuccess,
-  onRegistrationSuccess,
-}: ModalSectionProps) => {
-  const [registerModal, setRegisterModal] = useState<boolean>(false)
-  const modalRef = useRef<HTMLDivElement>(null)
+const ModalSection = observer(
+  ({
+    isOpen,
+    onClose,
+    onLoginSuccess,
+    onRegistrationSuccess,
+  }: ModalSectionProps) => {
+    const [registerModal, setRegisterModal] = useState<boolean>(false)
+    const modalRef = useRef<HTMLDivElement>(null)
 
-  const handleRoleChange = (newRole: TRole) => {
-    roleStore.role = newRole
-  }
-
-  const handleRegisterModal = () => {
-    setRegisterModal(!registerModal)
-  }
-
-  const handleLoginSuccess = (user: IUserContextData) => {
-    userStore.setUser(user)
-
-    if (onLoginSuccess) {
-      onLoginSuccess(user)
+    const handleRoleChange = (newRole: TRole) => {
+      roleStore.role = newRole
     }
-    onClose()
-  }
 
-  const handleRegistrationSuccess = (user: IUserData) => {
-    if (onRegistrationSuccess) {
-      onRegistrationSuccess(user)
+    const handleRegisterModal = () => {
+      setRegisterModal(!registerModal)
     }
-    setRegisterModal(true)
-  }
 
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent): void => {
-      if (
-        modalRef.current &&
-        !modalRef.current.contains(event.target as Node)
-      ) {
-        onClose()
+    const handleLoginSuccess = (user: IUserContextData) => {
+      userStore.setUser(user)
+
+      if (onLoginSuccess) {
+        onLoginSuccess(user)
       }
+      onClose()
     }
 
-    if (isOpen) {
-      document.addEventListener('mousedown', handleClickOutside)
-      document.body.style.overflow = 'hidden'
-    }
-
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside)
-      document.body.style.overflow = 'unset'
-    }
-  }, [isOpen, onClose])
-
-  useEffect(() => {
-    const handleEscape = (event: KeyboardEvent) => {
-      if (event.key === 'Escape') {
-        onClose()
+    const handleRegistrationSuccess = (user: IUserData) => {
+      if (onRegistrationSuccess) {
+        onRegistrationSuccess(user)
       }
+      setRegisterModal(true)
     }
 
-    if (isOpen) {
-      document.addEventListener('keydown', handleEscape)
+    useEffect(() => {
+      const handleClickOutside = (event: MouseEvent): void => {
+        if (
+          modalRef.current &&
+          !modalRef.current.contains(event.target as Node)
+        ) {
+          onClose()
+        }
+      }
+
+      if (isOpen) {
+        document.addEventListener('mousedown', handleClickOutside)
+        document.body.style.overflow = 'hidden'
+      }
+
+      return () => {
+        document.removeEventListener('mousedown', handleClickOutside)
+        document.body.style.overflow = 'unset'
+      }
+    }, [isOpen, onClose])
+
+    useEffect(() => {
+      const handleEscape = (event: KeyboardEvent) => {
+        if (event.key === 'Escape') {
+          onClose()
+        }
+      }
+
+      if (isOpen) {
+        document.addEventListener('keydown', handleEscape)
+      }
+
+      return () => {
+        document.removeEventListener('keydown', handleEscape)
+      }
+    }, [isOpen, onClose])
+
+    if (!isOpen) {
+      return null
     }
 
-    return () => {
-      document.removeEventListener('keydown', handleEscape)
-    }
-  }, [isOpen, onClose])
-
-  if (!isOpen) {
-    return null
-  }
-
-  return (
-    <div className="modal">
-      <div className="modal-content" ref={modalRef}>
-        <h3 className="modal-content__title">Вход в аккаунт</h3>
-        <p>Выберите тип учётной записи</p>
-        <ToggleButton onRoleChange={handleRoleChange} />
-        <RoleDescription role={roleStore.role} />
-        {registerModal ? (
-          <RegistrationField
-            role={roleStore.role as TRole}
-            onSuccess={handleRegistrationSuccess}
-            onSwitchToLogin={() => setRegisterModal(false)}
-          />
-        ) : (
-          <LoginField onSuccess={handleLoginSuccess} />
-        )}
-        <div
-          className="modal-content__registration"
-          onClick={handleRegisterModal}>
-          {!registerModal ? (
-            <div>
-              Нет аккаунта? <span>Зарегистрируйтесь</span>
-            </div>
+    return (
+      <div className="modal">
+        <div className="modal-content" ref={modalRef}>
+          <h3 className="modal-content__title">Вход в аккаунт</h3>
+          <p>Выберите тип учётной записи</p>
+          <ToggleButton onRoleChange={handleRoleChange} />
+          <RoleDescription role={roleStore.role} />
+          {registerModal ? (
+            <RegistrationField
+              role={roleStore.role as TRole}
+              onSuccess={handleRegistrationSuccess}
+              onSwitchToLogin={() => setRegisterModal(false)}
+            />
           ) : (
-            <div>
-              Есть аккаунт? <span>Войдите</span>
-            </div>
+            <LoginField onSuccess={handleLoginSuccess} />
           )}
+          <div
+            className="modal-content__registration"
+            onClick={handleRegisterModal}>
+            {!registerModal ? (
+              <div>
+                Нет аккаунта? <span>Зарегистрируйтесь</span>
+              </div>
+            ) : (
+              <div>
+                Есть аккаунт? <span>Войдите</span>
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
+)
 
-export default observer(ModalSection)
+export default ModalSection
